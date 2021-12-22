ef0fabde1071e70c7a45855ca05b347c250f4e7b
Alessandro <alessanbiz@gmail.com>
2021-12-21 23:16:41 -0300

    feign rest client added and load balancer with ribbon

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,8 +1,10 @@
 package br.com.alura.microservice.loja.service;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,13 +14,10 @@ import org.springframework.web.client.RestTemplate;
 public class CompraService {
 
     @Autowired
-    private RestTemplate client;
+    private FornecedorClient fornecedorClient;
 
     public void realizaCompra(CompraDTO compra) {
-        ResponseEntity<InfoFornecedorDTO> exchange =
-                client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
-                HttpMethod.GET, null, InfoFornecedorDTO.class);
-
-        System.out.println(exchange.getBody().getEndereco());
+        InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+        System.out.println(info.getEndereco());
     }
 }
