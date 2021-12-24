7441d7fb72cbc412ce8b7a0a72332231ff11ed76
Alessandro <alessanbiz@gmail.com>
2021-12-23 23:34:10 -0300

    feat: new database models for microservices and new feign client in loja

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
@@ -1,16 +1,24 @@
 package br.com.alura.microservice.fornecedor.service;
 
+import br.com.alura.microservice.fornecedor.controller.InfoController;
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
+import br.com.alura.microservice.fornecedor.repository.InfoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InfoService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InfoService.class);
+
+
     @Autowired
     private InfoRepository infoRepository;
 
     public InfoFornecedor getInfoPorEstado(String estado) {
+        LOG.info("informações buscadas");
         return infoRepository.findByEstado(estado);
     }
 }
--- loja/src/main/java/br/com/alura/microservice/loja/client/FornecedorClient.java
+++ loja/src/main/java/br/com/alura/microservice/loja/client/FornecedorClient.java
@@ -1,13 +1,21 @@
 package br.com.alura.microservice.loja.client;
 
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.dto.ItemDaCompraDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @FeignClient("fornecedor")
 public interface FornecedorClient {
 
     @RequestMapping("/info/{estado}")
     InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+
+    @PostMapping("/pedido")
+    InfoPedidoDTO realizaPedido(List<ItemDaCompraDTO> itens);
 }
